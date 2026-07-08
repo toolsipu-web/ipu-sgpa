075fbe7130d7d01feb6052b51c4ebaf4877c8472
pulkitchopra3007 <yopulkit13@gmail.com>
2026-07-08 13:43:05 +0530

    Update page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Calculator from "@/components/Calculator";
+import Formula from "@/components/Formula";
+import FAQ from "@/components/FAQ";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Calculator />
+      <Formula />
+      <FAQ />
+    </>
+  );
+}
